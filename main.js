806be1ae6cf11643110ba21ba06495cd93836a85
Kevin Pei <kevin-pei@hotmail.com>
2016-11-25 14:14:08 -0500

    lmao

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    name: 'FatalEagle',
+    picture: 'http://puu.sh/su19X/dd3704cadb.jpg',
+    primaryColor: 'yellow',
+    secondaryColor: 'black'
+  }
+})
