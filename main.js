c3157f42a77339da75a723abc73f43f1e5efc47b
Alex Foley <foleyfactor98@gmail.com>
2016-11-25 14:50:30 -0500

    Added _GET function

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+function _GET(addr) {
+	addr = decodeURI(addr.slice(addr.indexOf('?')+1)).split('&');
+	var params = {};
+	for (var i=0; i<addr.length; i++) {
+		var vals = addr[i].split('=');
+		params[vals[0]] = vals[1];
+	}
+	return params;
+}
+
+var params = _GET(document.location.toString());
+
 var app = new Vue({
   el: '#app',
   data: {
